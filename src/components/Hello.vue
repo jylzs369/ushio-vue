84139977a943a53ea7d2db1d4d9821af17954cd3
Ushio <jylzs369@163.com>
2017-08-12 23:57:32 +0800

    modify files

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -7,11 +7,6 @@
 <script>
 export default {
   data () {
-    return {
-      // note: changing this line won't causes changes
-      // with hot-reload because the reloaded component
-      // preserves its current state and we are modifying
-      // its initial state.
       msg: 'Hello World!'
     }
   }
